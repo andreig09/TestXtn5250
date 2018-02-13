020d53d99f070cdba29434b6f2f32c52020072d3
andreig09 <andreig09@gmail.com>
2018-02-13 18:24:31 -0300

    Adding a method to get the screen as a string

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ public class Main {
         //XITelnet tn = new XITelnet("rikas.rikascom.net");
         XI5250Emulator em  = new XI5250Emulator();
         em.setHost("rikas.rikascom.net");
-        em.setTerminalType("IBM-3179-2");
+        em.setTerminalType("IBM-3477-FC");
         XI5250EmulatorCtrl emctrl = new XI5250EmulatorCtrl(em);
 
         try {
@@ -18,13 +18,16 @@ public class Main {
                 System.out.println("System connected");
             }
 
-            Thread.sleep(10000);
+            Thread.sleep(5000);
 
             takeSnapShot(em);
+
+            System.out.println(getScreenAsString(em));
 
             em.setActive(false);
             if (!em.isActive()){
                 System.out.println("System disconnected");
+                return;
             }
         }
         catch (Exception ex) {
@@ -40,6 +43,19 @@ public class Main {
         frm.setBounds(0, 0, 728, 512);
         frm.centerOnScreen();
         frm.setVisible(true);
+    }
+
+    private static String getScreenAsString(XI5250Emulator emu){
+        int height = emu.getCrtSize().height;
+        int width = emu.getCrtSize().width;
+        String screen = "";
+
+        for (int i=0;i<height;i++){
+            screen += emu.getString(0,i,width);
+            screen += "\n";
+        }
+
+        return screen;
     }
 
 
